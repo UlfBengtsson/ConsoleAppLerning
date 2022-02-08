73da19dc0753d2e1ed116df8e067ab3c7b99ee08
Ulf Bengtsson <Ulf.Bengtsson@Lexicon.se>
2022-02-08 10:55:53 +0100

    Add project files.

---
+++ ConsoleAppLerning/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleAppLerning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepAlive = true;
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Run excersise by entering the number or -1 to exit.");
+
+                    int selection = int.Parse(Console.ReadLine() ?? "0");
+
+                    switch(selection)
+                    {
+                        case 1:
+                            Exercise1();
+                            break;
+                        //more cases as you progress doing the excersises.
+
+                        case -1:
+                            keepAlive = false;
+                            Console.WriteLine("Thanks for using the program.");
+                            break;
+                        default:
+                            Console.WriteLine("Was not a valid selection.");
+                            break;
+                    }
+
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey(true);
+                    Console.Clear();
+                }
+                catch
+                {
+                    Console.WriteLine("Exception has occured.");
+                }
+
+            } while (keepAlive);
+        }//main method
+
+        static void Exercise1()
+        {
+            Console.WriteLine("replace this with exercies");
+        }
+
+    }//class
+}//namespace
